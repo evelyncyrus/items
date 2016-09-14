05e93a5596113adbc4aedac716d929bef7954217
evelyn <evelyn@qq.com>
2016-09-14 18:19:11 +0800

    111

--- static/js/dialog.js
+++ static/js/dialog.js
@@ -1,77 +1,59 @@
 //弹出层方法
-var popup = function(arr) {
-  this.arr = arr;
-}
-var that = document.body;
-popup.alert = function(e, arr) {
-    var url = arr.url || '',
-      txt = arr.txt,
-      time = arr.interval || 1500,
-      node = e.creatElement('section');
-    dom = '<p><em><img src="' + url + '" alt=""></em><strong>' + txt + '</strong></p>';
-    node.className = 'popup popup-alert', node.innerHTMl = dom, that.appendChild(node),
-      setTimeout(function() {
-        that.removeChild(node);
-      }, time);
-  },
-  popup.confirm = function(e, arr) {
-    var url = arr.url || '',
-      txt = arr.txt,
-      cancel = arr.cancelInfo,
-      confirm = arr.confirmInfo,
-      fun = arr.callback,
-      node = e.creatElement('section'),
-      dom = "<p><em><img src=" + url + ' alt=""></em><strong>' + txt + '</strong></p><a class="popup-confirm-cancel" href="javascript:void(0);">' + cancel + '</a><a class="popup-confirm-confirm" href="javascript:void(0);">' + confirm + "</a>";
-    node.className = 'popup popup-confirm', node.innerHTMl = dom, that.appendChild(node), node.addEventLister('click', function(e) {
-      var item = e.target;
-      'popup-confirm-cancel' == item.className.toLowerCase() ? (that.removeChild(node), mask.hide()) : 'popup-confirm-confirm ' == item.className.toLowerCase() && (that.removeChild(node), mask.hide(), fun && fun())
-    });
-  },
-  popup.mask = function(e) {
-    var node = e.creatElement('div');
-    return node.className = 'mask', node.style.background = 'rgba(0,0,0,.4)', node.style.position = 'fixed', node.style.left = '0', node.styld.top = '0', node.style.width = '100%', n.style.height = '100%', node.style.zIndex = '500', node.style.display = 'none', that.appendChild(node), {
-      show: function() {
-        node.style.display = 'block';
-      },
-      hide: function() {
-        node.style.display = 'none';
+function createPopup(obj) {
+  var popup = new Object;
+  popup.time = obj.interval || 3000,
+    popup.root = obj.goal || '',
+    popup.url = obj.url || '',
+    popup.txt = obj.txt || '',
+    popup.body = popup.root.body || '',
+    popup.cancel = obj.cancelInfo || '',
+    popup.confirm = obj.confirmInfo || '',
+    popup.fun = obj.fun || '',
+    popup.alert = function(obj) {
+      var node = this.root.createElement('section'),
+        dom = '<p><em><img src="' + url + '" alt=""></em><strong>' + txt + '</strong></p>',
+        pop = this.body.getElementsByClassName('popup');
+      if(pop.length < 1) {
+        popup.mask(obj),
+          node.className = 'popup popup-alert',
+          node.innerHTML = dom,
+          this.body.appendChild(node),
+          setTimeout(function() {
+            this.body.removeChild(node);
+          }, time);
+      } else {
+        return false;
       }
+    },
+    popup.confirm = function(obj) {
+      node = this.root.createElement('section'),
+        dom = "<p><em><img src=" + url + ' alt=""></em><strong>' + txt + '</strong></p><a class="popup-confirm-cancel" href="javascript:void(0);">' + cancel + '</a><a class="popup-confirm-confirm" href="javascript:void(0);">' + confirm + "</a>";
+      node.className = 'popup popup-confirm', node.innerHTMl = dom, this.body.appendChild(node), node.addEventLister('click', function(e) {
+        var item = e.target;
+        'popup-confirm-cancel' == item.className.toLowerCase() ? (that.removeChild(node), mask.hide()) : 'popup-confirm-confirm ' == item.className.toLowerCase() && (that.removeChild(node), mask.hide(), fun && fun())
+      });
+    },
+    popup.mask = function(obj) {
+      var node = root.createElement('div');
+      return node.className = 'mask',
+        node.style = {
+          'background': 'rgba(0,0,0,.4)',
+          'position': 'fixed',
+          'left': '0',
+          'top': '0',
+          'width': '100%',
+          'height': '100%',
+          'zIndex': '500'
+        }, this.body.appendChild(node), {
+          show: function() {
+            node.style.display = 'block',
+              setTimeout(function() {
+                this.body.removeChild(this);
+              }, time);
+          },
+          hide: function() {
+            node.style.display = 'none';
+          }
+        }
     }
-  }
-
-
-// popup.alert = function(arr) {
-//     var url = arr.url || '',
-//       txt = arr.txt,
-//       time = arr.interval || 1500,
-//       node = document.body.creatElement('section'),
-//       dom = '<p><em><img src="' + url + '" alt=""></em><strong>' + txt + '</strong></p>';
-//     node.className = 'popup popup-alert', node.innerHTMl = dom, that.appendChild(node),
-//       setTimeout(function() {
-//         that.removeChild(node);
-//       }, time);
-//   },
-//   popup.confirm = function(arr) {
-//     var url = arr.url || '',
-//       txt = arr.txt,
-//       cancel = arr.cancelInfo,
-//       confirm = arr.confirmInfo,
-//       fun = arr.callback,
-//       node = document.creatElement('section'),
-//       dom = "<p><em><img src=" + url + ' alt=""></em><strong>' + txt + '</strong></p><a class="popup-confirm-cancel" href="javascript:void(0);">' + cancel + '</a><a class="popup-confirm-confirm" href="javascript:void(0);">' + confirm + "</a>";
-//     node.className = 'popup popup-confirm', node.innerHTMl = dom, that.appendChild(node), node.addEventLister('click', function(e) {
-//       var item = e.target;
-//       'popup-confirm-cancel' == item.className.toLowerCase() ? (that.removeChild(node), mask.hide()) : 'popup-confirm-confirm ' == item.className.toLowerCase() && (that.removeChild(node), mask.hide(), fun && fun())
-//     });
-//   }
-// popup.mask = function(e) {
-//   var node = document.creatElement('div');
-//   return node.className = 'mask', node.style.background = 'rgba(0,0,0,.4)', node.style.position = 'fixed', node.style.left = '0', node.styld.top = '0', node.style.width = '100%', n.style.height = '100%', node.style.zIndex = '500', node.style.display = 'none', that.appendChild(node), {
-//     show: function() {
-//       node.style.display = 'block';
-//     },
-//     hide: function() {
-//       node.style.display = 'none';
-//     }
-//   }
-// }
+}
