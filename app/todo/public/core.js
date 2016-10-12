82835ef73f60abb5fdeb79586d43bdfb3b5076c4
evelyn <evelyn@qq.com>
2016-10-12 15:53:33 +0800

    app todo list

--- app/todo/public/core.js
+++ app/todo/public/core.js
@@ -4,17 +4,19 @@ function mainController($scope, $http) {
   $scope.formData = {};
 
   // when landing on the page, get all todos and show them
-  $http.get('/api/todos')
-    .success(function(data) {
-      console.log(data);
-      $scope.todos = data;
-    }).error(function(data) {
-      console.log('Error: ' + data);
-    });
+  $scope.readTodo = function() {
+    $http.get('/todos')
+      .success(function(data) {
+        // console.log(data)
+        $scope.todos = data;
+      }).error(function(data) {
+        console.log('Error: ' + data);
+      });
+  }
 
   // when submitting the add form, send the text to the node API
   $scope.createTodo = function() {
-    $http.post('/api/todos', $scope.formData)
+    $http.post('/todos', $scope.formData)
       .success(function(data) {
         $scope.formData = {};
         $scope.todos = data;
@@ -26,7 +28,7 @@ function mainController($scope, $http) {
 
   // delete a todo after checking it
   $scope.deleteTodo = function(id) {
-    $http.delete('/api/todos/' + id)
+    $http.delete('/todos/' + id)
       .success(function(data) {
         $scope.todos = data;
       })
--- app/todo/server.js
+++ app/todo/server.js
@@ -1,73 +1,72 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/todo');
 
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
+app.use(express.static('./public'));
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
-  // log every request to the console
-  app.use(express.logger('dev'));
-
-  // pull information from html in POST
-  app.use(express.urlencoded());
-  app.use(express.json());
-});
 
 //---define model
 var Todo = mongoose.model('Todo', {
-  text: String
+  text: {
+    type: String,
+    default: ''
+  }
 });
 
-app.get('*',function(req,res){
-  res.sendfile('./public/index.html');
-})
-
-
-//---define routes
-//get all todos
-app.get('/api/todos', function(req, res) {
-  //use mongoose to get all todos in the datebase
+function getTodos(res) {
   Todo.find(function(err, todos) {
-    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-    if(err) { res.send(err); }
+    if(err) {
+      res.send(err);
+    }
     res.json(todos); // return all todos in JSON format
   });
+}
+
+// ----- define routes
+// get all todos
+app.get('/todos', function(req, res) {
+  getTodos(res);
 });
 
-// create Todo and send back all todos after creation
-app.post('/api/todos', function(req, res) {
+//create Todo and send back all todos after creation
+app.post('/todos', function(req, res) {
+  // create a Todo, information comes from AJAX request from Angular
   Todo.create({
     text: req.body.text,
     done: false
   }, function(err, todo) {
-    if(err) { res.send(err) }
-    // get and return all the todos after you create another
-    Todo.find(function(err, todos) {
-      if(err) {
-        res.send(err);
-      }
-      res.json(todos);
-    })
-  });
-});
-
-app.delete('/api/todos/:todo_id', function(req, res) {
-  Todo.remove({
-    _id: req.params.todo_id
-  }, function(err, todo) {
     if(err) {
       res.send(err);
     }
-    Todo.find(function(err, todos) {
-      if(err) {
-        res.send(err);
-      }
-      res.json(todos);
-    })
-  })
-})
+    // get and return all the todos after you create another
+    getTodos(res);
+  });
+});
+// delete a todo
+app.delete('/todos/:todo_id', function(req, res) {
+  Todo.remove({
+    _id: req.params.todo_id
+  }, function(err, todo) {
+    if(err) { res.send(err); }
+    // get and return all the todos after you create another
+    getTodos(res);
+  });
+});
+
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 
 app.listen(8080);
 console.log('app listening on port 8080');
